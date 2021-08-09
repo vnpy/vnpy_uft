a000a8a5d0889226c226727b9413c0f784f172e7
vn.py <xiaoyou.chen@foxmail.com>
2021-08-09 13:50:01 +0800

    [Mod] 更新版本号

--- vnpy_uft/__init__.py
+++ vnpy_uft/__init__.py
@@ -29,4 +29,7 @@ except ImportError:
 import importlib_metadata
 
 
-__version__ = importlib_metadata.version("vnpy_uft")
+try:
+    __version__ = importlib_metadata.version("vnpy_uft")
+except importlib_metadata.PackageNotFoundError:
+    __version__ = "dev"
